8fa4755b6bee25664bce3984dbf69d27db0be6f7
devgorilla <dwbrown89@gmail.com>
2018-08-20 08:37:12 -0700

    EJ-EA Radiator Writeup + shorter disclaimer + linked Authors on writeup pages, also testing gulp as replacement for renderer.js

---
+++ renderer/gulp-test.js
@@ -0,0 +1,16 @@
+var handlebars = require('gulp-handlebars');
+var wrap = require('gulp-wrap');
+var declare = require('gulp-declare');
+var concat = require('gulp-concat');
+
+gulp.task('templates', function(){
+  gulp.src('detail-pages/*.hbs')
+    .pipe(handlebars())
+    .pipe(wrap('Handlebars.template(<%= contents %>)'))
+    .pipe(declare({
+      namespace: 'MyApp.templates',
+      noRedeclare: true, // Avoid duplicate declarations
+    }))
+    .pipe(concat('templates.js'))
+    .pipe(gulp.dest('build/test'));
+});
